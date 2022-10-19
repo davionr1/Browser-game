77581fdf60d71f6c9551a28bcd9bde7e6384781d
Davion Roachford <davionr1@icloud.com>
2022-10-19 01:17:53 -0700

    make js functions to start game with timer

---
+++ play.js
@@ -0,0 +1,10 @@
+const char = document.querySelector('.character-1')
+char.addEventListener('click', ()=>{
+    char.style.backgroundColor = "red";
+})
+// async function startGame(){
+    
+//     await document.addEventListener('click', event=>{
+//         alert("hi");
+//    });
+// }
